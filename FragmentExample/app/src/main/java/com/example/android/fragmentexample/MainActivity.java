52030c391a72af281a0aad4790b01effdeac96fd
Izolentiy <nima.munkonov@gmail.com>
2020-10-22 02:28:37 +0800

    Fragment example: dynamic

--- FragmentExample/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
+++ FragmentExample/app/src/main/java/com/example/android/fragmentexample/MainActivity.java
@@ -16,14 +16,74 @@
 
 package com.example.android.fragmentexample;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String STATE_FRAGMENT = "state_of_fragment";
+
+    private Button mButton;
+    private boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+            if (isFragmentDisplayed)
+                mButton.setText(R.string.close);
+        }
+        mButton = findViewById(R.id.open_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFragmentDisplayed)
+                    closeFragment();
+                else
+                    displayFragment();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
+        super.onSaveInstanceState(outState);
+    }
+
+    public void displayFragment() {
+        SimpleFragment fragment = SimpleFragment.newInstance();
+        // Get the FragmentManager and start a transaction.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // Add a SimpleFragment.
+        fragmentTransaction.add(R.id.fragment_container, fragment).addToBackStack(null).commit();
+        // Update the Button text.
+        mButton.setText(R.string.close);
+        // Set boolean flag to indicate fragment is open.
+        isFragmentDisplayed = true;
+    }
+
+    public void closeFragment() {
+        // Get the fragment manager.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        // Check to see if the fragment is already showing.
+        SimpleFragment simpleFragment = (SimpleFragment)
+                fragmentManager.findFragmentById(R.id.fragment_container);
+        if (simpleFragment != null) {
+            // Create and commit the transaction to remove the fragment.
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(simpleFragment).commit();
+        }
+        // Update the Button text.
+        mButton.setText(R.string.open);
+        // Set the boolean flag to indicate fragment is close.
+        isFragmentDisplayed = false;
     }
 }
