7b3ebe9d8e3bc1228100fa4e8525581b1ec57baa
Izolentiy <nima.munkonov@gmail.com>
2020-10-31 23:24:45 +0800

    StackedViews app: with overdraw

---
+++ StackedViews/app/src/main/java/learning/android/stackedviews/MainActivity.kt
@@ -0,0 +1,13 @@
+package learning.android.stackedviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.view1).background.alpha = 150
+    }
+}
