58be2f9819ad8ea7855c82e46d0aa6135bcc701e
Izolentiy <nima.munkonov@gmail.com>
2020-11-01 22:43:41 +0800

    StackedViews app: fixed overdraw

--- StackedViews/app/src/main/java/learning/android/stackedviews/MainActivity.kt
+++ StackedViews/app/src/main/java/learning/android/stackedviews/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.view1).background.alpha = 150
+        findViewById<TextView>(R.id.view1).alpha = 0.3f
+        findViewById<TextView>(R.id.view2).alpha = 0.6f
+        findViewById<TextView>(R.id.view3).alpha = 0.9f
     }
 }
